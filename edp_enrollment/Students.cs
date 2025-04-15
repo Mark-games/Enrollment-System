c97b80fccb05ef84e709e14dd843ecdf8df92a80
Mark-games <mjcb2022-5740-67472@bicol-u.edu.ph>
2025-04-15 18:13:28 +0800

    Add files via upload

---
+++ edp_enrollment/Students.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace edp_enrollment
+{
+    public partial class Students : Form
+    {
+        public Students()
+        {
+            InitializeComponent();
+        }
+
+        private void close9_btn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void studback_btn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Dashboard back7 = new Dashboard();
+            back7.Show();
+        }
+    }
+}
